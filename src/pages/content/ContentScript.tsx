f5f5e7137080224a883070168d8ed3f0fcf03dbc
YarikMix <43493788+YarikMix@users.noreply.github.com>
2025-03-15 13:18:39 +0300

    feat: Добавил замену текста на странице по хоткею

--- src/pages/content/ContentScript.tsx
+++ src/pages/content/ContentScript.tsx
@@ -1,5 +1,9 @@
 import useExtensionState from "@pages/hooks/useExtensionState";
 import {useEffect} from "react";
+import hotkeys from "hotkeys-js";
+import ReactDOMServer from "react-dom/server"
+import TextNode from "@pages/content/TextNode";
+import axios from "axios";
 
 const ContentScript = () => {
 	const {color} = useExtensionState();
@@ -7,9 +11,41 @@ const ContentScript = () => {
 	console.log("ContentScript")
 	console.log("color", color)
 
+	function switchText() {
+		// Gets the selection range
+		// This is from Tim Down, linked below
+		let range, sel = window.getSelection();
+		if (sel.rangeCount && sel.getRangeAt) {
+			range = sel.getRangeAt(0);
+		}
+
+		// Creates a new node range
+		document.designMode = "on";
+		if (range) {
+			sel.removeAllRanges();
+			sel.addRange(range);
+		}
+
+		// You can use either a variable or a string
+		const someNewText = "-- You can make this whatever you want --";
+
+		// This is from user YeppThatsMe, also linked below
+		// document.execCommand("insertHTML", false, `<span id='${uuid}' style="background: blue">`+ document.getSelection()+"</span>");
+		document.execCommand("insertHTML", false, ReactDOMServer.renderToStaticMarkup(<TextNode text={someNewText}></TextNode>));
+
+		document.designMode = "off";
+	};
+
 	useEffect(() => {
-		document.body.style.background = color
-	}, [color]);
+		hotkeys('g', (e) => {
+			e.preventDefault()
+
+			axios('https://jsonplaceholder.typicode.com/todos/1')
+				.then(resp => console.log(resp.data))
+
+			switchText()
+		});
+	}, []);
 
 	return (
 		<div>
